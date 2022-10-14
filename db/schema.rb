5636fc4052127fa70334a918b35090109bd44167
(Benjamin Olivera) <unknowsoldierbj@gmail.com>
2022-10-14 17:18:55 -0500

    adding user_name column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_12_205405) do
+ActiveRecord::Schema.define(version: 2022_10_14_221424) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2022_10_12_205405) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_name"
   end
 
 end
