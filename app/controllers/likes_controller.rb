88dc2fe26116cd1f5c8cd3a1a0f88c6d3e7da87e
(Benjamin Olivera) <unknowsoldierbj@gmail.com>
2022-10-22 10:09:40 -0500

    view for likes.html.erb

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -19,9 +19,4 @@ class LikesController < ApplicationController
     redirect_to("/posts/#{params[:post_id]}")
   end
 
-  def likes
-    @user = User.find_by(id: params[:id])
-    @likes = Like.where(user_id: @user.id)
-  end
-
 end
